ed557dd1f60294957e211318445d1068f532ecf6
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-03-29 13:52:46 +0200

    rename route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,7 @@ import { register, signIn } from '../controllers/user.controller'
 const routes = (app) => {
     app.route('/api/auth/register')
         .post(register)
-    app.route('/api/auth/signin')
+    app.route('/api/auth/login')
         .post(signIn)
 }
 
